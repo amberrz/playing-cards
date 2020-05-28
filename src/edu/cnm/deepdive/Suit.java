af41096b4751ed6e2cb02c8bda623dae9ae82b0b
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-05-28 11:07:05 -0600

    created shoe (ncl. shuffle) & ShoeTester.

--- src/edu/cnm/deepdive/Suit.java
+++ src/edu/cnm/deepdive/Suit.java
@@ -1,9 +1,19 @@
 package edu.cnm.deepdive;
 
+/**
+ * Encapsulate the suits of standard playing cards. each instance can return (using the
+ *{@link #symbol()} method) the Unicode symbol for that suit.
+ * method) the Unicode symbol for that suit.
+ */
 public enum Suit {
 
   CLUBS, DIAMONDS, HEARTS, SPADES;
 
+  /**
+   * Return the Unicode symbol for the current instance.
+   *
+   * @return One of the four symbols: ♠, ♡, ♢, ♣.
+   */
   public char symbol() {
     switch(this) {
       case CLUBS:
