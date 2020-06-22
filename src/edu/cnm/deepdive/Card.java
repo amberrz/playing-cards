e86a4be763dc121f1719ba418878a7c91ac81234
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-06-22 09:42:16 -0600

    Implemeted Comparable and Comparator

--- src/edu/cnm/deepdive/Card.java
+++ src/edu/cnm/deepdive/Card.java
@@ -1,6 +1,6 @@
 package edu.cnm.deepdive;
 
-public class Card {
+public class Card implements Comparable<Card> {
 
   private final Suit suit;
   private final Rank rank;
@@ -13,6 +13,7 @@ public class Card {
    */
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
+
     this.rank = rank;
   }
 
@@ -29,4 +30,12 @@ public class Card {
     return rank.symbol() + suit.symbol();
   }
 
+  @Override
+  public int compareTo(Card other) {
+    int comparison = suit.compareTo(other.suit);
+    if (comparison == 0){
+      comparison = rank.compareTo(other.rank);
+    }
+    return comparison;
+  }
 }
