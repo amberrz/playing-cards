e9c0a968771bcb4f68c9a8e337ebf7c8c28d2661
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-06-22 12:01:39 -0600

    Implemeted Comparable and Comparator

--- src/edu/cnm/deepdive/Suit.java
+++ src/edu/cnm/deepdive/Suit.java
@@ -1,13 +1,21 @@
 package edu.cnm.deepdive;
 
 /**
- * Encapsulate the suits of standard playing cards. each instance can return (using the
- *{@link #symbol()} method) the Unicode symbol for that suit.
- * method) the Unicode symbol for that suit.
+ * Encapsulate the suits of standard playing cards. each instance can return (using the {@link
+ * #symbol()} method) the Unicode symbol for that suit. method) the Unicode symbol for that suit.
  */
 public enum Suit {
 
-  CLUBS, DIAMONDS, HEARTS, SPADES;
+  CLUBS(Color.BLACK),
+  DIAMONDS(Color.RED),
+  HEARTS(Color.RED),
+  SPADES(Color.BLACK);
+
+  private final Color color;
+
+  private Suit(Color color) {
+    this.color = color;
+  }
 
   /**
    * Return the Unicode symbol for the current instance.
@@ -15,17 +23,21 @@ public enum Suit {
    * @return One of the four symbols: ♠, ♡, ♢, ♣.
    */
   public char symbol() {
-    switch(this) {
+    switch (this) {
       case CLUBS:
         return '\u2663';
       case DIAMONDS:
         return '\u2662';
       case HEARTS:
-      return '\u2661';
+        return '\u2661';
       case SPADES:
         return '\u2660';
       default:
-      return 0;
+        return 0;
     }
+  }
+
+  public Color getColor() {
+    return color;
   }
 }
